de2f42797c94385b83d038c6218da4ccfec0a62e
John <j.adams440@gmail.com>
2014-04-26 05:02:53 +0100

    Initial commit

---
+++ dev/js/main.js
@@ -0,0 +1,26 @@
+var ld = ld || {};
+
+window.onload = function() {
+
+    ld.canvas = document.getElementById('gameCanvas');
+    ld.canvas.width = 1080;
+    ld.canvas.height = 720;
+    ld.ctx = ld.canvas.getContext('2d');
+    ld.container = document.getElementById('gameContainer');
+
+    var vendors = [
+        'ms',
+        'moz',
+        'webkit',
+        'o'
+    ];
+    for (var x = 0; x < vendors.length && !window.requestAnimationFrame; x++)
+    {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'];
+    }
+
+    ld.ctx.fillStyle = '#000000';
+    ld.ctx.fillRect(0,0,ld.canvas.width,ld.canvas.height);
+
+};
